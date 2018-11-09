5bac5c6460f44dc16bcef576f1c65177a20f58a0
Frank Fishburn <frankthefishburn@gmail.com>
2018-11-08 22:41:03 -0400

    Cap maximum amplitude in shader

--- shaders/vert_direct_freq.glsl
+++ shaders/vert_direct_freq.glsl
@@ -7,6 +7,6 @@ void main(void){
 
     float x_pos = 1.95 * (float(gl_VertexID) - float(num_freq)/2.0) / float(num_freq);
 
-    gl_Position = vec4(x_pos, multiplier*amplitude*0.99, 1.0, 1.0);
+    gl_Position = vec4(x_pos, multiplier*0.99*min(amplitude,1.0), 1.0, 1.0);
 
 })"
