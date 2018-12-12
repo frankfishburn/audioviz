49523b722297fedaaf90ee6f10e06fbf77a33214
Frank Fishburn <frankthefishburn@gmail.com>
2018-12-11 23:46:42 -0500

    Switch to coloring by frequency rather than power

--- shaders/frag_plain.glsl
+++ shaders/frag_plain.glsl
@@ -7,17 +7,17 @@ out vec4 FragColor;
 void main(void) {
 
     float xpos = 2.0 * abs(gl_FragCoord.x - resolution.x/2.0) / resolution.x; 
-    float ypos = 2.0 * abs(gl_FragCoord.y - resolution.y/2.0) / resolution.y;
+    float ypos = abs(gl_FragCoord.y) / resolution.y;
 
     // Apply the jet colormap
     float R, G, B;
-    R = 4.0*xpos - 2.0;
-    if (xpos<0.25) {
-        G = 4.0*xpos;
+    R = 4.0*ypos - 2.0;
+    if (ypos<0.5) {
+        G = 4.0*ypos;
     } else {
-        G = -4.0*xpos + 4.0;
+        G = -4.0*ypos + 4.0;
     }
-    B = -4.0*xpos + 2.0;
+    B = -4.0*ypos + 2.0;
 
     FragColor = vec4( R, G, B, 1.0 );
 }
