05f60dfeb876fdf26f931e2a49b6f07886a024db
Frank Fishburn <frankthefishburn@gmail.com>
2019-01-23 23:33:03 -0500

    Improve comments in main vertex shader

--- shaders/vert_direct_freq.glsl
+++ shaders/vert_direct_freq.glsl
@@ -6,6 +6,7 @@ void main(void){
  
     int vertexID;
 
+    // Obtain vertex ID corrected for triangle strip layout
     if (gl_VertexID < 2*(num_freq+1)) {
 
         // Left channel
@@ -18,8 +19,14 @@ void main(void){
         
     }
     
+    // Convert corrected vertex ID to frequency index
     float freq = floor(float(vertexID)/2.0);
-    float y_pos = 1.95 * (freq - float(num_freq)/2.0) / float(num_freq);
+
+    // Normalize frequency to [-1,1]
+    freq = 2.0 * (freq - float(num_freq)/2.0) / float(num_freq);
+
+    // Clamp values
+    float y_pos = 0.99 * freq;
     float x_pos = 0.99 * min(amplitude,1.0);
     
     gl_Position = vec4( x_pos, y_pos, 0.0, 1.0);
